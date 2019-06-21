d6fd145a687f7654b40bbbb969e695ecadc20c39
drsk0211 <47639604+drsk0211@users.noreply.github.com>
2019-06-21 16:39:58 +0300

    1

--- send.php
+++ send.php
@@ -1,29 +1,39 @@
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$f = $_POST['f'];
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
-$message = htmlspecialchars($message);
-$f = htmlspecialchars($f);
-$name = urldecode($name);
-$email = urldecode($email);
-$message = urldecode($message);
-$f = urldecode($f);
-$name = trim($name);
-$email = trim($email);
-$message = trim($message);
-$f = trim($f);
-echo $name;
-echo "<br>";
-echo $email;
-echo $message;
-echo "<br>";
-echo $f;
-mail("dr0211@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n");
-if (mail("dr0211@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n");)
- { 
-    echo "сообщение успешно отправлено"; 
-} else { 
-    echo "при отправке сообщения возникли ошибки"; 
-}
+
+<?php
+    $send = $_POST['button'];
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $text = $_POST['text'];
+    $f = $_POST['f'];
+ 
+    if ($send != "button"){
+        $to = "dr0211l@mail.ru";                 
+        $subject = "New message"; 
+ 
+        $mailheaders = "From: " . strip_tags($_POST['mail']) . "\r\n";
+        $mailheaders .= "Reply-To: ". strip_tags($_POST['mail']) . "\r\n";
+        $mailheaders .= "CC: dr0211@mail.ru\r\n"; //отправка копии
+ 
+        //$mailheaders .= "MIME-Version: 1.0\r\n";
+        $mailheaders .= "Content-Type: text/html; charset=utf-8\r\n"; //charset=ISO-8859-1
+ 
+        $message = '<html><body>';
+        $message .= "<p style='background: #eee;'><strong>Отправитель:</strong> " . strip_tags($_POST['name']) . "</p>";
+        $message .= "<p style='background: #eee;'><strong>Email:</strong> " . strip_tags($_POST['mail']) . "</p>";
+        $message .= "<p style='background: #eee;'><strong>Текст сообщения:</strong> " . strip_tags($_POST['text']) . "</p>";
+        $message .= "</body></html>";
+ 
+        mail ($to, $subject, $message, $mailheaders);
+        echo "<html><head>"; 
+        echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+        echo "<title>Отправлено</title>"; 
+        echo "</head>"; 
+        echo "<body>"; 
+        echo "<p id=successful_message>Здравствуйте $name, ваше сообщение было успешно отправлено.</p>"; 
+        echo "<div>"; 
+        echo '<a href="../contacts.html" id=link_send_back>Вернуться</a>';
+        echo "</div>"; 
+        echo "</body>"; 
+        echo "</html>";
+    }
+?>
